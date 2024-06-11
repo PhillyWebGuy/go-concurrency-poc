b0cab89e8094a96ddc533bd05756f00cb1ce2cc9
Ed Hertzog <edhertzog@gmail.com>
2024-06-11 12:56:48 -0400

    Remove CompileDaemon related functionality

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"context"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 	"v2/internal/initializers"
 
 	"github.com/gin-gonic/gin"
@@ -29,42 +24,7 @@ func main() {
 		})
 	})
 
-	//This creates a new HTTP server on port 8080 with the Gin router as the handler.
-	srv := &http.Server{
-		Addr:    ":" + os.Getenv("PORT"),
-		Handler: r,
-	}
+	//This line starts the server on port env port. The server will listen for incoming requests and respond to them.
+	r.Run(":" + os.Getenv("PORT"))
 
-	//starts the HTTP server in a new goroutine. The server will start serving incoming requests.
-	//If the server fails to start, it will panic.
-	go func() {
-		// service connections
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			panic(err)
-		}
-	}()
-
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
-	quit := make(chan os.Signal)
-	// kill (no param) default send syscall.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	//This line blocks the program from exiting until it receives a signal.
-	//When it receives a signal, it proceeds to the next line.
-	<-quit
-	//This line prints a message indicating that the server is shutting down.
-	println("Shutting down server...")
-
-	//This creates a context that will be canceled after 5 seconds.
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	//This attempts to gracefully shut down the server within the 5-second timeout. If it fails, it panics.
-	if err := srv.Shutdown(ctx); err != nil {
-		panic("Server forced to shutdown:")
-	}
-
-	//This prints a message indicating that the server has exited.
-	println("Server exiting")
 }
