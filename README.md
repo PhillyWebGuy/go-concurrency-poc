e9b937281e9e5190edd69c982a447d0592aef2b2
Ed Hertzog <edhertzog@gmail.com>
2024-06-11 12:11:18 -0400

    Add comments, gitignore, README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# go-concurrency-poc
-A Go Concurrency POC
+# GO-CONCONCURRENCY-POC
+
+The purpose of this POC is to demonstrate the use of:
+
+- CompileDaemon
+- GIN
+- REST API that consumes and produces JSON
+- Gorm
+- Concurrency
+
+# Starting the Application
+
+cmd/http/CompileDaemon -build="go build ./main.go" -command="./main"
+
+Point your browser: http://localhost:8080
+
+# Branches / Milestones
+
+## setup
+
+This contains the commits that do the following:
+
+- Add main, git ignore, basic directory structure
+- Add postgres support
+- Add Gorm support
+- Add Gin
+- Add godotenv
+- Add CompileDaemon
--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//This creates a new Gin router with default middleware: logger and recovery (crash-free) middleware.
+	r := gin.Default()
+
+	//defines a route handler for HTTP GET requests to the root ("/") URL. When someone accesses the root URL,
+	//the server responds with a JSON object: {"message": "ping pang pung wut up dawg"}.
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping pang pung wut up dawg",
+		})
+	})
+
+	//This creates a new HTTP server on port 8080 with the Gin router as the handler.
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	//starts the HTTP server in a new goroutine. The server will start serving incoming requests.
+	//If the server fails to start, it will panic.
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	//This line blocks the program from exiting until it receives a signal.
+	//When it receives a signal, it proceeds to the next line.
+	<-quit
+	//This line prints a message indicating that the server is shutting down.
+	println("Shutting down server...")
+
+	//This creates a context that will be canceled after 5 seconds.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	//This attempts to gracefully shut down the server within the 5-second timeout. If it fails, it panics.
+	if err := srv.Shutdown(ctx); err != nil {
+		panic("Server forced to shutdown:")
+	}
+
+	//This prints a message indicating that the server has exited.
+	println("Server exiting")
 }
