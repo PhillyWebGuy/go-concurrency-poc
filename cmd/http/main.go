6829001cb9ca6caf1761273d8a195e72680def8d
Ed Hertzog <edhertzog@gmail.com>
2024-06-13 11:57:21 -0400

    Added a rudimentary handler

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -15,5 +15,13 @@ func RunHTTP() {
 
 	r := gin.Default()
 
+	//This defines a route handler for HTTP GET requests to the root ("/") URL. When someone accesses the root URL,
+	//the server responds with a JSON object: {"message": "ping pang pung wut up dawg"}.
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping pang pung wut up dawg",
+		})
+	})
+
 	log.Fatal(r.Run(config.HTTP.Port))
 }
